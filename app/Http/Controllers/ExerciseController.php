a2667d2e514e7ae292acbfe2d798bf59c6d6d0ce
Donlegend1 <shedrackogwuche5@gmail.com>
2026-09-15 09:40:48 +0100

    updated

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -225,9 +225,7 @@ class ExerciseController extends Controller
     {
         $skillLevel = $request->query('skill_level', 'ALL');
         $skillLevels = ['ALL', 'Beginner', 'Intermediate', 'Advanced'];
-        $search = $request->query('name');Uploaded Learn Songs videos not showing in admin 
-
-Group Etudes & Pieces Lessons Like Extra course and Guided Practice lesson page view
+        $search = $request->query('name');
 
         $categoryPage = \App\Models\MusicalApplicationCategory::query()
             ->when($skillLevel !== 'ALL', fn ($q) => $q->where('level', strtolower($skillLevel)))
